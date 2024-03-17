f5670526b07e58e25cfaf4bbdca151cfaa48f681
NSTLRD <20210416@itla.edu.do>
2024-03-17 12:41:19 -0400

    feature to CRUD operations in a product

--- src/main/java/com/mentorly/inventorybe/controller/ProductController.java
+++ src/main/java/com/mentorly/inventorybe/controller/ProductController.java
@@ -13,10 +13,9 @@ import com.mentorly.inventorybe.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,9 +24,69 @@ public class ProductController {
 
     private final ProductService productService;
 
+    /**
+     * @param productCreateDTO
+     * @return ProductDTO
+     * @since 3/16/2024
+     * @version inventory-be 1.0
+     * This method creates a product
+    **/
     @PostMapping
     public ResponseEntity<ProductDTO> createProduct(@RequestBody ProductCreateDTO productCreateDTO) {
         ProductDTO productDTO = productService.createProduct(productCreateDTO);
         return new ResponseEntity<>(productDTO, HttpStatus.CREATED);
     }
+
+    /**
+     * @return List of all products
+     * @since 3/16/2024
+     * @version inventory-be 1.0
+     * This method returns a list of all products
+    **/
+    @GetMapping
+    public ResponseEntity<List<ProductDTO>> getAllProduct() {
+        List<ProductDTO> products = productService.listAllProduct();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    /**
+     * @param id
+     * @return ProductDTO
+     * @since 3/16/2024
+     * @version inventory-be 1.0
+     * This method returns a product by its id
+     **/
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO> getProductById(@PathVariable Long id) {
+        ProductDTO productDTO = productService.getProductById(id);
+        return new ResponseEntity<>(productDTO, HttpStatus.OK);
+    }
+
+    /**
+     * @param id
+     * @param productCreateDTO
+     * @return ProductDTO
+     * @since 3/16/2024
+     * @version inventory-be 1.0
+     * This method updates a product by its id
+     **/
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDTO> updateProduct(@PathVariable Long id, @RequestBody ProductCreateDTO productCreateDTO) {
+        ProductDTO productDTO = productService.updateProduct(id, productCreateDTO);
+        return new ResponseEntity<>(productDTO, HttpStatus.OK);
+    }
+
+    /**
+     * @param id
+     * @since 3/16/2024
+     * @version inventory-be 1.0
+     * This method deletes a product by its id
+     **/
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
 }
--- src/main/java/com/mentorly/inventorybe/service/ProductService.java
+++ src/main/java/com/mentorly/inventorybe/service/ProductService.java
@@ -9,6 +9,13 @@ package com.mentorly.inventorybe.service;
 import com.mentorly.inventorybe.dto.ProductCreateDTO;
 import com.mentorly.inventorybe.dto.ProductDTO;
 
+import java.util.List;
+
 public interface ProductService {
     ProductDTO createProduct(ProductCreateDTO productCreateDTO);
+
+    List<ProductDTO> listAllProduct();
+    ProductDTO getProductById(Long productId);
+    ProductDTO updateProduct(Long productId, ProductCreateDTO productCreateDTO);
+    void deleteProduct(Long productId);
 }
